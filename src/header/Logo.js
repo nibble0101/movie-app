aab5763c42633d287f064f0802d46abb0a0dd20d
nibble0101 <mj.mawa@yahoo.com>
2021-04-12 18:36:02 +0300

    Make app name a link to the landing page

--- src/header/Logo.js
+++ src/header/Logo.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function Logo(props) {
   return (
     <div className="header-logo">
       <h1 className="logo-text">
-        <span className="logo-text-1">Film</span>
-        <span className="logo-text-2">Maze</span>
+        <a href="/" className="link">
+          <span className="logo-text-1">Film</span>
+          <span className="logo-text-2">Maze</span>
+        </a>
       </h1>
     </div>
   );
