2351f471bde33f9c610e85cb5eb41d381becbdda
nibble0101 <mj.mawa@yahoo.com>
2021-04-12 19:57:26 +0300

    Add formatting

--- src/header/Menu.js
+++ src/header/Menu.js
@@ -1,19 +1,23 @@
-import React, {useCallback, useState} from "react";
+import React, { useCallback, useState } from "react";
 import HamburgerMenu from "react-hamburger-menu";
 import { NavLink } from "react-router-dom";
 
 function Menu(props) {
   const [isOpen, setIsOpen] = useState(false);
   const myRef = React.useRef();
+
   const hambugerClickHandler = useCallback(() => {
     setIsOpen(!isOpen);
     myRef.current.classList.toggle("display-menu");
   }, [isOpen]);
-  const listClickHandler = useCallback((e) => {
-    myRef.current.classList.toggle("display-menu");
-    setIsOpen(!isOpen);
 
-  }, [isOpen]);
+  const listClickHandler = useCallback(
+    (e) => {
+      myRef.current.classList.toggle("display-menu");
+      setIsOpen(!isOpen);
+    },
+    [isOpen]
+  );
   return (
     <div className="header-menu">
       <ul
@@ -23,13 +27,27 @@ function Menu(props) {
         ref={myRef}
       >
         <li id="movies">
-          <NavLink exact to = {{pathname: "/", state: {genreId: 0}}} activeClassName = "active-menu" > Movies </NavLink>
+          <NavLink
+            exact
+            to={{ pathname: "/", state: { genreId: 0 } }}
+            activeClassName="active-menu"
+          >
+            Movies
+          </NavLink>
         </li>
         <li id="tv">
-          <NavLink exact to = {{ pathname: "/tvshows/all", state: {genreId: 0}}} activeClassName = "active-menu">TV Shows</NavLink>
+          <NavLink
+            exact
+            to={{ pathname: "/tvshows/all", state: { genreId: 0 } }}
+            activeClassName="active-menu"
+          >
+            TV Shows
+          </NavLink>
         </li>
         <li id="people">
-          <NavLink exact to = "/people" activeClassName = "active-menu">People</NavLink>
+          <NavLink exact to="/people" activeClassName="active-menu">
+            People
+          </NavLink>
         </li>
       </ul>
       <HamburgerMenu
