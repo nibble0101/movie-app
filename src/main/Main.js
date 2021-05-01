b1d50b3cde1a86f6deff50b2f72f4d5dc04003b4
nibble0101 <mj.mawa@yahoo.com>
2021-05-01 19:13:35 +0300

    Change number of columns

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,28 +6,36 @@ import MovieDetails from "./movies/details/MovieDetails";
 import TvShowDetails from "./tvshows/details/TvShowDetails";
 import PeopleDetails from "./people/details/PeopleDetails";
 import MovieSearchResults from "./movies/search/MovieSearchResults";
-import TvSearchResults  from "./tvshows/search/TvSearchResults";
+import TvSearchResults from "./tvshows/search/TvSearchResults";
 import PeopleSearchResults from "./people/search/PeopleSearchResults";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function Main(props) {
   return (
     <div className="display">
       <Switch>
-        <Route exact path="/" render={({location}) => {
-        const state = !location.state ? {genreId: 0} : location.state;
-        return <Redirect to={{pathname: "/movies/all", state: state}} />
-        }} 
-        />
+        <Route exact path="/" component={Movies} />
         <Route exact path="/tvshows/:genre" component={TvShows} />
         <Route exact path="/tvshows/:genre/:id" component={TvShowDetails} />
         <Route exact path="/people" component={People} />
         <Route exact path="/people/:id" component={PeopleDetails} />
         <Route exact path="/movies/:genre" component={Movies} />
-        <Route exact path = "/movies/:genre/:id" component = {MovieDetails} />
-        <Route exact path = "/movies/search/all/:name" component = {MovieSearchResults} />
-        <Route exact path = "/tvshows/search/all/:name" component = {TvSearchResults} />
-        <Route exact path = "/people/search/:name" component = {PeopleSearchResults} />
+        <Route exact path="/movies/:genre/:id" component={MovieDetails} />
+        <Route
+          exact
+          path="/movies/search/all/:name"
+          component={MovieSearchResults}
+        />
+        <Route
+          exact
+          path="/tvshows/search/all/:name"
+          component={TvSearchResults}
+        />
+        <Route
+          exact
+          path="/people/search/:name"
+          component={PeopleSearchResults}
+        />
       </Switch>
     </div>
   );
